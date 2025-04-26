52aa36c50227a93e21e86ee5f0debea3ed26fb0e
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2025-04-26 15:23:26 +0800

    rubah warna font hero

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
   return (
     <div
       id="home"
-      className="hero px-1 md:px-[10%] dark:text-black bg-white flex items-center justify-center min-h-screen"
+      className="hero px-1 md:px-[10%]  bg-white flex items-center justify-center min-h-screen"
     >
       <div className="hero-content w-full flex-col lg:flex-row-reverse">
         <div className="flex px-1 justify-center items-center w-full">
@@ -17,12 +17,14 @@ export const Hero = () => {
           />
         </div>
         <div>
-          <h1 className="text-2xl opacity-80 font-bold">The art of brewing!</h1>
-          <h1 className="text-3xl lg:text-5xl font-bold">
+          <h1 className="text-2xl text-black opacity-80 font-bold">
+            The art of brewing!
+          </h1>
+          <h1 className="text-3xl text-black lg:text-5xl font-bold">
             Find your moment at <br />
             <span className="text-[#E60000] font-climate-crisis">REDLANE.</span>
           </h1>
-          <p className="py-6">
+          <p className="py-6 text-black">
             Selamat datang di Red Lane Coffee!, coffee shop yang hadir bukan
             hanya untuk menyajikan kopi terbaik, tetapi juga untuk merangkai
             momen bermakna bersama setiap tegukan. Kami percaya, setiap cangkir
